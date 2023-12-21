4bed821a4bd3410b05dfd3e87407a3a32c01149f
BriannaCoalCar <44736324+briannadrew@users.noreply.github.com>
2023-12-20 23:39:01 -0500

    Added connectivity with ITFFF, connected front and backend, design updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,63 @@
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faIceCream } from '@fortawesome/free-solid-svg-icons';
-import { faBoltLightning } from '@fortawesome/free-solid-svg-icons';
-import { faBowlFood } from '@fortawesome/free-solid-svg-icons';
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faIceCream } from "@fortawesome/free-solid-svg-icons";
+import { faBoltLightning } from "@fortawesome/free-solid-svg-icons";
+import { faBowlFood } from "@fortawesome/free-solid-svg-icons";
+const ip_address = "192.168.1.223";
+const date = "12/20/2023";
 
 function App() {
+  async function icecream() {
+    try {
+      await axios.post("http://" + ip_address + ":8000/post_tone1");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function harrypotter() {
+    try {
+      await axios.post("http://" + ip_address + ":8000/post_tone2");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function feed() {
+    try {
+      await axios.post("http://" + ip_address + ":8000/schedule", {
+        date,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <div className='title'>
-        <h1>Pet-'O-Matic</h1>
+      <div className="title">
+        <h1>PET-'O-MATIC</h1>
       </div>
-      <div className='tone'>
-        <div className='tone-caption'>
-          <h2>Select a Ringtone</h2>
+      <div className="tone">
+        <div className="tone-caption">
+          <h2>♫ Select a Ringtone ♫</h2>
         </div>
-        <div className='tone-buttons'>
-          <button id='icecream'>
+        <div className="tone-buttons">
+          <button id="icecream" onClick={icecream}>
             <FontAwesomeIcon icon={faIceCream} />
           </button>
-          <button id='harrypotter'>
+          <button id="harrypotter" onClick={harrypotter}>
             <FontAwesomeIcon icon={faBoltLightning} />
           </button>
         </div>
       </div>
-      <div className='feed-button'>
-        <div className='feed-caption'>
-          <h2>Feed Your Pet!</h2>
+      <div className="feed-button">
+        <div className="feed-caption">
+          <h2>🐾 Feed Your Pet! 🐾</h2>
         </div>
-        <button id='feeder'>
+        <button id="feeder" onClick={feed}>
           <FontAwesomeIcon icon={faBowlFood} />
         </button>
       </div>
--- server/server.js
+++ server/server.js
@@ -1,14 +1,43 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const IFTTT = require("node-ifttt-maker");
+const ifttt = new IFTTT(process.env.IFTTT_KEY);
+const eventname1 = "RingTone1";
+const eventname2 = "RingTone2";
+const eventname3 = "Schedule";
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/message', (req, res) => {
-    res.json({ message: "Hello from the server!" });
+app.post("/post_tone1", async (req, res) => {
+  ifttt
+    .request(eventname1)
+    .then((response) => {})
+    .catch((err) => {});
+});
+
+app.post("/post_tone2", async (req, res) => {
+  ifttt
+    .request(eventname2)
+    .then((response) => {})
+    .catch((err) => {});
+});
+
+app.post("/schedule", async (req, res) => {
+  let { date } = req.body;
+  ifttt
+    .request(eventname3, date)
+    .then((response) => {})
+    .catch((err) => {});
+});
+
+app.get("/message", (req, res) => {
+  res.json({ message: "Hello from the server!" });
 });
 
 app.listen(8000, () => {
-    console.log('Server is running on port 8000.');
+  console.log("Server is running on port 8000.");
 });
